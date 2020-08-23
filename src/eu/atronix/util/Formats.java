eceb19d52b2f800e452dc205105b0ccf797c8031
Steven-Hendrik Kriebel <49077834+Atronix1902@users.noreply.github.com>
2020-08-23 22:24:20 +0200

    adding future ideas and deleting useless comments

--- src/eu/atronix/util/Formats.java
+++ src/eu/atronix/util/Formats.java
@@ -1,6 +1,21 @@
 package eu.atronix.util;
 
+/**
+ * Provides some Formatting functions
+ * <br>such as formatting to economic Strings
+ * @author Atronix
+ * @version 1.0
+ *
+ */
 public class Formats {
+	/*
+	 * IDEAS:
+	 * set number of when to set point, dividing char/string via parameter 
+	 */
+	
+	
+	
+	
 	/**
 	 * @author Atronix
 	 * @param number double - the number that should be converted to an economic number
@@ -15,14 +30,10 @@ public class Formats {
 		StringBuilder strArr0 = new StringBuilder();
 		
 		if(arrInt[0].length() > 3) {
-			
 			for(int i = 3; i < arrInt[0].length(); i+=3) {
 				String extract = arrInt[0].subSequence(arrInt[0].length() - i, arrInt[0].length() - i + 3).toString();
 				strArr0.insert(0, '.' + extract);
 			}
-			
-			//removes the '.' in front of first digit
-			//strArr0.delete(0, 0);
 		} else {
 			strArr0.insert(0, arrInt[0].toString());
 		}
